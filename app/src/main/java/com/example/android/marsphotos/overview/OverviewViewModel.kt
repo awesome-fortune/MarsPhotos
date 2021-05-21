2d71ce51b9edf9d991555fb56a4ef413e7b9eeaa
Fortune Maseko <mrfortune.maseko@gmail.com>
2021-05-21 02:27:34 +0200

    Display connection status images

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -9,9 +9,11 @@ import com.example.android.marsphotos.network.MarsPhoto
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
 class OverviewViewModel : ViewModel() {
-    private val _status = MutableLiveData<String>()
-    val status: LiveData<String> = _status
+    private val _status = MutableLiveData<MarsApiStatus>()
+    val status: LiveData<MarsApiStatus> = _status
 
     private val _photos = MutableLiveData<List<MarsPhoto>>()
     val photos: LiveData<List<MarsPhoto>> = _photos
@@ -22,11 +24,14 @@ class OverviewViewModel : ViewModel() {
 
     private fun getMarsPhotos() {
         viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
+
             try {
                 _photos.value = MarsApi.retrofitService.getPhotos()
-                _status.value = "Success: Mars properties retrieved"
+                _status.value = MarsApiStatus.DONE
             } catch (e: Exception) {
-                _status.value = "Failure: ${e.message}"
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
             }
         }
     }
